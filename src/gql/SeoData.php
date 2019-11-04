a759fb2ef0537441dccdca6ae4361d8f79cb7049
Freek van Rijt <freek@digitalnatives.nl>
2019-11-04 14:27:22 +0100

    Fix sub-object implementations

--- src/gql/SeoData.php
+++ src/gql/SeoData.php
@@ -1,13 +1,11 @@
 <?php
 namespace ether\seo\gql;
 
-use craft\base\VolumeInterface;
-use craft\gql\base\InterfaceType;
 use craft\gql\GqlEntityRegistry;
 use craft\gql\interfaces\elements\Asset as AssetInterface;
-use craft\gql\types\elements\Asset;
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
+use craft\helpers\Gql;
 
 class SeoData extends \craft\gql\base\ObjectType
 {
@@ -16,33 +14,17 @@ class SeoData extends \craft\gql\base\ObjectType
      */
     public static function getName(): string
     {
-        return 'SeoData2';
+        return self::class;
     }
 
     /**
      * @inheritdoc
      */
-    public static function getType(): self
+    public static function getType(): Type
     {
         if ($type = GqlEntityRegistry::getEntity(self::class)) {
             return $type;
         }
-
-        $socialFieldObject = new ObjectType([
-            'name' => 'SEO Social Data',
-            'description' => 'Social data for an individual Social network',
-            'fields' => [
-                'title' => [
-                    'type' => Type::string(),
-                ],
-                'image' => [
-                    'type' => AssetInterface::getType(),
-                ],
-                'description' => [
-                    'type' => Type::string(),
-                ]
-            ]
-        ]);
 
         return GqlEntityRegistry::createEntity(self::class, new \GraphQL\Type\Definition\ObjectType([
             'name' => static::getName(),
@@ -57,12 +39,95 @@ class SeoData extends \craft\gql\base\ObjectType
                     'name' => 'SEO Keyword',
                     'fields' => ['keyword' => Type::string(), 'rating' => Type::string()]
                 ])),
-                'social' => new ObjectType([
-                    'name' => 'SEO social',
-                    'twitter' => $socialFieldObject,
-                    'facebook' => $socialFieldObject
-                ]),
+                'social' => SeoSocialNetworks::getType()
             ]
         ]));
+    }
+}
+
+class SeoSocialNetworks extends \craft\gql\base\ObjectType {
+    /**
+     * @inheritdoc
+     */
+    public static function getName(): string
+    {
+        return self::class;
+    }
+
+    public static function getType(): ObjectType {
+        if ($type = GqlEntityRegistry::getEntity(self::class)) {
+            return $type;
+        }
+
+        return GqlEntityRegistry::createEntity(self::class, new \GraphQL\Type\Definition\ObjectType([
+            'name' => static::getName(),
+            'fields' => [
+                'twitter' => SeoSocialData::getType(),
+                'facebook' => SeoSocialData::getType()
+            ]
+        ]));
+    }
+
+}
+
+
+class SeoSocialData extends \craft\gql\base\ObjectType {
+    /**
+     * @inheritdoc
+     */
+    public static function getName(): string
+    {
+        return self::class;
+    }
+
+    public static function getType(): ObjectType {
+        if ($type = GqlEntityRegistry::getEntity(self::class)) {
+            return $type;
+        }
+
+        return GqlEntityRegistry::createEntity(self::class, new \GraphQL\Type\Definition\ObjectType([
+            'name' => static::getName(),
+            'description' => 'Social data for an individual Social network',
+            'fields' => array_merge(self::getConditionalFields(), [
+                'title' => [
+                    'type' => Type::string(),
+                ],
+                'description' => [
+                    'type' => Type::string(),
+                ]
+            ])
+        ]));
+    }
+
+    /**
+     * Get fields which may only be used depending on the craft Gql config
+     */
+    protected static function getConditionalFields(): array
+    {
+        // Images may be in any public volume, so verify them all.
+        $volumes = \Craft::$app->volumes->getPublicVolumes();
+        $awareOfAllPublicVolumes = false;
+
+        if (!empty($volumes)) {
+            foreach ($volumes as $volume) {
+                if (!Gql::isSchemaAwareOf('volumes.' . $volume->uid)) {
+                    $awareOfAllPublicVolumes = false;
+                    break;
+                } else {
+                    $awareOfAllPublicVolumes = true;
+                }
+            }
+        }
+
+        if ($awareOfAllPublicVolumes) {
+            return [
+                'image' => [
+                    'name' => 'image',
+                    'type' => AssetInterface::getType(),
+                ]
+            ];
+        }
+
+        return [];
     }
 }
