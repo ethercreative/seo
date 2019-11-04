338b2a73b4d5b57d2160d7859405911e30cb1219
Freek van Rijt <freek@digitalnatives.nl>
2019-11-04 11:26:54 +0100

    Switch all ObjectTypes to Craft object types

--- src/gql/SeoData.php
+++ src/gql/SeoData.php
@@ -5,7 +5,6 @@ use craft\base\VolumeInterface;
 use craft\gql\base\InterfaceType;
 use craft\gql\interfaces\elements\Asset as AssetInterface;
 use craft\gql\types\elements\Asset;
-use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 
 class SeoData extends \craft\gql\base\ObjectType
@@ -21,9 +20,9 @@ class SeoData extends \craft\gql\base\ObjectType
     /**
      * @inheritdoc
      */
-    public static function getType(): ObjectType
+    public static function getType(): self
     {
-        $socialFieldObject = new ObjectType([
+        $socialFieldObject = new self([
             'name' => 'SEO Social Data',
             'description' => 'Social data for an individual Social network',
             'fields' => [
@@ -41,7 +40,7 @@ class SeoData extends \craft\gql\base\ObjectType
             ]
         ]);
 
-        return new ObjectType([
+        return new self([
             'name' => static::getName(),
             'fields' => [
                 'title' => [
@@ -52,11 +51,11 @@ class SeoData extends \craft\gql\base\ObjectType
                     'type' => Type::string(),
                     'resolve' => static function($value) { return html_entity_decode($value); }
                 ],
-                'keywords' => Type::listOf(new ObjectType([
+                'keywords' => Type::listOf(new self([
                     'name' => 'SEO Keyword',
                     'fields' => ['keyword' => Type::string(), 'rating' => Type::string()]
                 ])),
-                'social' => new ObjectType([
+                'social' => new self([
                     'name' => 'SEO social',
                     'twitter' => $socialFieldObject,
                     'facebook' => $socialFieldObject
